fabb42e7a6940ba0f7709e90a3a5d73c0ad1cdf0
pranavan <184130e@uom.lk>
2022-01-19 15:47:29 +0530

    initializing repository

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Login from './components/login';
+import Header from './components/header';
+import Landing from './components/landing';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* <Login/> */}
+      <Landing/>
+      
+    </div>
+  );
+}
+
+export default App;
